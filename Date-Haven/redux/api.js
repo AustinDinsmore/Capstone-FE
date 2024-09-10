6f2506d47909f10a60f177ca00ac54cce82564db
Austin.Dinsmore <austdins@DESKTOP-C5NFOS3>
2024-09-09 19:15:56 -0600

    deploying

--- Date-Haven/redux/api.js
+++ Date-Haven/redux/api.js
@@ -4,7 +4,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const dates_api = createApi({
     reducerPath: "dates_api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:8080",
+        baseUrl: "https://date-haven.onrender.com",
     }),
     endpoints: (builder) => ({
         register: builder.mutation({
